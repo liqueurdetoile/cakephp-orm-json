3b223e129510564e559d5737c8ce7bb896490ef5
Liqueur de Toile <contact@liqueurdetoile.com>
2022-07-08 17:26:38 +0200

    chore: Remove global bootstrap to avoid mischarging things in prod

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+use Cake\Cache\Cache;
+use Cake\Chronos\Chronos;
+use Cake\Core\Configure;
+use Cake\Datasource\ConnectionManager;
+use Cake\Log\Log;
+use Cake\Utility\Security;
 use Migrations\TestSuite\Migrator;
 
 /**
@@ -10,13 +16,6 @@ use Migrations\TestSuite\Migrator;
  * has been installed as a dependency of the plugin, or the plugin is itself
  * installed as a dependency of an application.
  */
-$dsn = env('TRAVIS', false) ?
-  'mysql://root:root@localhost/cakeormjson_test' :
-  'mysql://root@localhost/cakeormjson_test';
-
-putenv('DB_URL=' . $dsn);
-putenv('TESTING=1');
-
 $findRoot = function ($root) {
     do {
         $lastRoot = $root;
@@ -32,11 +31,96 @@ $findRoot = function ($root) {
 $root = $findRoot(__FILE__);
 unset($findRoot);
 
-if (file_exists($root . '/config/bootstrap.php')) {
-    include $root . '/config/bootstrap.php';
+if (is_file('vendor/autoload.php')) {
+    include_once 'vendor/autoload.php';
 } else {
-    include $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
+    include_once dirname(__DIR__) . '/vendor/autoload.php';
 }
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+define('ROOT', dirname(__DIR__));
+define('APP_DIR', 'TestApp');
+
+define('TMP', sys_get_temp_dir() . DS);
+define('LOGS', ROOT . DS . 'logs' . DS);
+define('CACHE', TMP . 'cache' . DS);
+define('SESSIONS', TMP . 'sessions' . DS);
+
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
+define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
+define('CORE_TESTS', CORE_PATH . 'tests' . DS);
+define('CORE_TEST_CASES', CORE_TESTS . 'TestCase');
+define('TEST_APP', ROOT . DS . 'tests' . DS);
+
+// Point app constants to the test app.
+define('APP', TEST_APP . 'TestApp' . DS);
+define('WWW_ROOT', TEST_APP . 'webroot' . DS);
+define('CONFIG', TEST_APP . 'config' . DS);
+
+// phpcs:disable
+@mkdir(LOGS);
+@mkdir(SESSIONS);
+@mkdir(CACHE);
+@mkdir(CACHE . 'views');
+@mkdir(CACHE . 'models');
+// phpcs:enable
+
+require_once CORE_PATH . 'config' . DS . 'bootstrap.php';
+
+date_default_timezone_set('UTC');
+mb_internal_encoding('UTF-8');
+
+Configure::write('debug', true);
+
+Cache::setConfig(
+    [
+    '_cake_core_' => [
+        'engine' => 'File',
+        'prefix' => 'cake_core_',
+        'serialize' => true,
+    ],
+    '_cake_model_' => [
+        'engine' => 'File',
+        'prefix' => 'cake_model_',
+        'serialize' => true,
+    ],
+    ]
+);
+
+// Ensure default test connection is defined
+$dsn = env('CI', false) ?
+  'mysql://root@localhost/cakeormjson_test' :
+  'mysql://root@localhost/cakeormjson_test';
+
+putenv('DB_URL=' . $dsn);
+putenv('TESTING=1');
+
+ConnectionManager::setConfig('test', ['url' => getenv('DB_URL')]);
+
+Log::setConfig(
+    [
+    'debug' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['notice', 'info', 'debug'],
+        'file' => 'debug',
+        'path' => LOGS,
+    ],
+    'error' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+        'file' => 'error',
+        'path' => LOGS,
+    ],
+    ]
+);
+
+// Basic for tests
+Chronos::setTestNow(Chronos::now());
+Security::setSalt('a-long-but-not-random-value');
 
 // Run migrations
 $migrator = new Migrator();
