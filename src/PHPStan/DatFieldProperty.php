49231e48b1aa9e564b4db33172ed8a894ed6c056
Liqueur de Toile <contact@liqueurdetoile.com>
2022-09-06 15:17:55 +0200

    style: Fix php-cs issue

--- src/PHPStan/DatFieldProperty.php
+++ src/PHPStan/DatFieldProperty.php
@@ -21,72 +21,114 @@ class DatFieldProperty implements PropertyReflection
      */
     private $type;
 
+    /**
+     * @param \PHPStan\Reflection\ClassReflection $declaringClass Owning class reflection
+     */
     public function __construct(ClassReflection $declaringClass)
     {
         $this->declaringClass = $declaringClass;
         $this->type = new MixedType();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getDeclaringClass(): ClassReflection
     {
         return $this->declaringClass;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isStatic(): bool
     {
         return false;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isPrivate(): bool
     {
         return false;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isPublic(): bool
     {
         return true;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isReadable(): bool
     {
         return true;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isWritable(): bool
     {
         return true;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isDeprecated(): TrinaryLogic
     {
         return TrinaryLogic::createNo();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getDeprecatedDescription(): ?string
     {
         return null;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isInternal(): TrinaryLogic
     {
         return TrinaryLogic::createNo();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getDocComment(): ?string
     {
         return null;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getReadableType(): Type
     {
         return $this->type;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getWritableType(): Type
     {
         return $this->type;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function canChangeTypeAfterAssignment(): bool
     {
         return true;
